1fb035f4bd0e0c770db1428b92fdedc7990991ff
arjun <arjunprakash027@gmail.com>
2023-09-05 19:14:36 +0530

    gpuutil changes

--- gpuinfo.py
+++ gpuinfo.py
@@ -4,12 +4,26 @@ def get_gpu_info():
   """Gets the GPU information."""
   gpus = GPUtil.getGPUs()
   gpu_info = []
-  for gpu in gpus:
+  if gpus:
+    for gpu in gpus:
+      gpu_info.append({
+          "gpu_name": gpu.name,
+          "memory_total": gpu.memoryTotal,
+          "memory_used": gpu.memoryUsed,
+          "temperature": gpu.temperature
+      })
+  else:
+
     gpu_info.append({
-        "gpu_name": gpu.name,
-        "memory_total": gpu.memoryTotal,
-        "memory_used": gpu.memoryUsed,
-        "temperature": gpu.temperature
-    })
+          "gpu_name": "No GPU",
+          "memory_total": 0,
+          "memory_used": 0,
+          "temperature": 0
+      })
+    
   return gpu_info
+
+if __name__ == "__main__":
+  print("GPU Info:")
+  print(get_gpu_info())
 
